5cf0fcf859b2206842071c6148438a9ec9a5a73a
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-08-16 20:52:04 +0200

    Initial Commit

---
+++ LearningSwiftUIByDoing/LearningSwiftUIByDoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIByDoingApp.swift
+//  LearningSwiftUIByDoing
+//
+//  Created by Cristina on 2023-08-16.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIByDoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
