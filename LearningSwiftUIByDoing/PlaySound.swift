b5f6cf808682377a00eaa7f5ff1f696e4e72755a
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-08-17 18:16:38 +0200

    create a audio player and add sound to assets

---
+++ LearningSwiftUIByDoing/PlaySound.swift
@@ -0,0 +1,24 @@
+//
+//  PlaySound.swift
+//  LearningSwiftUIByDoing
+//
+//  Created by Cristina on 2023-08-17.
+//
+
+import Foundation
+import AVFoundation
+
+// MARK: - AUDIO PLAYER
+
+var audioPlayer: AVAudioPlayer?
+
+func playSound(sound: String, type: String) {
+    if let path = Bundle.main.path(forResource: sound, ofType: type) {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+            audioPlayer?.play()
+        } catch {
+            print("ERROR: Could not find and play the sound file.")
+        }
+    }
+}
