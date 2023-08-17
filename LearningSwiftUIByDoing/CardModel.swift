b0e6f7e293f378303a74b13105142ab30f7c5d9e
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2023-08-17 17:33:52 +0200

    add card data model

---
+++ LearningSwiftUIByDoing/CardModel.swift
@@ -0,0 +1,19 @@
+//
+//  CardModel.swift
+//  LearningSwiftUIByDoing
+//
+//  Created by Cristina on 2023-08-17.
+//
+
+import SwiftUI
+
+// MARK: - CARD MODEL
+struct Card: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var imageName: String
+    var callToAction: String
+    var message: String
+    var gradientColors: [Color]
+}
